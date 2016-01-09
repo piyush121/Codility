ca626f11fa0303cd2325d585c6a78fa96d2b5066
Piyush Chaudhary <piyushchaudhary07@gmail.com>
2016-01-09 16:17:08 -0500

    hello

---
+++ CountDistinctSlices.java
@@ -0,0 +1,59 @@
+package Codility;
+import java.util.*;
+/* An integer M and a non-empty zero-indexed array A consisting of N non-negative integers are given.
+ *  All integers in array A are less than or equal to M.
+
+A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A.
+The slice consists of the elements A[P], A[P + 1], ..., A[Q].
+A distinct slice is a slice consisting of only unique numbers. 
+That is, no individual number occurs more than once in the slice.
+
+For example, consider integer M = 6 and array A such that:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 5
+    A[3] = 5
+    A[4] = 2
+There are exactly nine distinct slices: (0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 2), (3, 3), (3, 4) and (4, 4).
+*/
+public class CountDistinctSlices {
+	
+	public static int solution(int M, int A[], int N)
+	{	int slices=0;
+		int j=0;
+		HashSet<Integer> myhash=new HashSet<>();
+		for(int i=0;i<A.length;)
+		{
+			if(!myhash.contains(A[i]))
+				{	
+					myhash.add(A[i]);
+					slices++;
+
+				}
+			
+			else 
+				{	
+					myhash.clear();
+					i=j;
+					j++;
+				}
+			if(i==A.length-1)
+			{
+				myhash.clear();
+				i=j;
+				j++;
+			}
+			i++;
+		}
+		
+		return slices;
+		
+	}
+
+	public static void main(String[] args)
+	{
+		int[] arr={3,4,5,5,2};
+		System.out.println(solution(6, arr, 5));
+	}
+}
