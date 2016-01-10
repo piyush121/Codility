8cc668ed572dd731e1baf577caf505c7c8e1513a
Piyush Chaudhary <piyushchaudhary07@gmail.com>
2016-01-10 15:33:18 -0500

    more excercise

---
+++ countTriangles.java
@@ -0,0 +1,31 @@
+package Codility;
+
+import java.util.Arrays;
+
+public class countTriangles {
+	public static int solution(int[] A)
+	{	
+		Arrays.sort(A);
+		int i,j,k,count=0;
+		for(i=0;i<A.length-2;i++)
+		{	
+			k=i+2;
+			for(j=i+1;j<A.length-1;j++)
+			{
+				while(k<A.length && A[i]+A[j]>A[k] )
+						{	
+							count+=k-j;
+							++k;
+						}
+				
+			}
+		}
+		return count;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] arr={10,2,5,1,8,12};
+		System.out.print(solution(arr));
+	}
+}
